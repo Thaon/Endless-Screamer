3f87ce4213eb22dfe571639957e088b3b9f9bf3f
YAP! <GMADDA200@caledonian.ac.uk>
2017-01-20 20:20:16 +0000

    initial commit

---
+++ Assets/Code/ScreamDetector.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class ScreamDetector : MonoBehaviour
+{
+
+    #region member variables
+
+    public float m_sensitivity;
+
+    private AudioSource m_audio;
+    private float[] m_samples;
+
+    #endregion
+
+    void Start()
+    {
+        m_audio = GetComponent<AudioSource>();
+
+        foreach (string device in Microphone.devices)
+        {
+            print(device);
+        }
+
+        m_audio.clip = Microphone.Start(Microphone.devices[0], true, 180, 44100);
+
+        while (!(Microphone.GetPosition(null) > 0))
+        {
+        }
+
+        m_audio.Play();
+
+        m_samples = new float[4096]; // 4096 = around 85 ms of samples
+    }
+
+    void Update()
+    {
+        float vol = GetRMS(0) + GetRMS(1);
+        vol *= m_sensitivity;
+
+        transform.position = new Vector3(0, vol, 0);
+        //Debug.Log("Vol:" + vol); // the actual intensity/ volume of the sound from the microphone	    
+    }
+
+    float GetRMS(int channel)
+    {
+        m_audio.GetOutputData(m_samples, channel); // fill array with samples
+        float sum = 0;
+        for (var i = 0; i < 4096; i++)
+        {
+            sum += m_samples[i] * m_samples[i]; // sum squared samples
+        }
+        return Mathf.Sqrt(sum / 4096); // rms = square root of average 
+    }
+}
+
