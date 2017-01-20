bd8cf6cc01ad507b6d4f7652f9e9853d2947b943
baligaandris <baligaandris@gmail.com>
2017-01-20 22:03:58 +0000

    Winning now works

--- Assets/Code/WinTrigger.cs
+++ Assets/Code/WinTrigger.cs
@@ -3,18 +3,21 @@ using System.Collections;
 
 public class WinTrigger : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private PersistentData m_pData;
+    private float m_speed;
+    // Use this for initialization
+    void Start () {
+        m_pData = FindObjectOfType<PersistentData>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        m_speed = m_pData.m_speed;
+        transform.Translate(new Vector3(-1, 0, 0) * m_speed * Time.deltaTime);
+    }
 
     void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == "Player") {
+        if (other.tag == "Player") {
             //game end
             Debug.Log("you win!!!");
         }
