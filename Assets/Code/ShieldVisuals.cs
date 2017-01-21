894de33f815e3068fa08fb4d6a308972bb35e69d
YAP! <GMADDA200@caledonian.ac.uk>
2017-01-21 13:24:52 +0000

    added shield visuals

---
+++ Assets/Code/ShieldVisuals.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldVisuals : MonoBehaviour {
+
+    public GameObject m_shield;
+
+    private PersistentData m_pData;
+
+    void Start()
+    {
+        m_pData = FindObjectOfType<PersistentData>();
+    }
+
+	void Update ()
+    {
+        m_shield.SetActive(m_pData.shielded);
+	}
+}
