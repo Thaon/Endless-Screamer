1eac8903ebda6df184cee9936bda1638e98763bb
baligaandris <baligaandris@gmail.com>
2017-01-21 13:17:49 +0000

    Flip Vertical Powerup works

---
+++ Assets/Code/Powerup_FlipVertical.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup_FlipVertical : MonoBehaviour {
+
+    #region member variables
+
+    private float m_speed;
+    private PersistentData m_pData;
+
+    #endregion
+
+    void Start()
+    {
+        m_pData = FindObjectOfType<PersistentData>();
+    }
+
+    void Update()
+    {
+        m_speed = m_pData.m_speed;
+        transform.Translate(new Vector3(-1, 0, 0) * m_speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject.FindObjectOfType<Camera>().transform.Rotate(0, 0, 180);
+        }
+    }
+}
