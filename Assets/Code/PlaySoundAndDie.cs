4deefbac98f3720cac484b77bd1ae17d9adb4992
YAP! <GMADDA200@caledonian.ac.uk>
2017-01-22 12:27:42 +0000

    reworked audio on collision again

---
+++ Assets/Code/PlaySoundAndDie.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AudioSource))]
+
+public class PlaySoundAndDie : MonoBehaviour {
+
+    private AudioSource m_audio;
+
+    void Start ()
+    {
+        m_audio = GetComponent<AudioSource>();
+
+    }
+	
+	IEnumerator PlayAndDie ()
+    {
+        yield return new WaitForSeconds(m_audio.clip.length);
+        Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Code/SpawnAudioObjectOnCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnAudioObjectOnCollision : MonoBehaviour {
+
+    public GameObject m_audioObject;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Instantiate(m_audioObject);
+    }
+}
